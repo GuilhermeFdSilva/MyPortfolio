48ba62c714862c53b5c985e5c742fde98ca7e2ef
Guilherme <francaguilherme27@gmail.com>
2023-11-16 14:56:09 -0300

    Lista links

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  projectLinks: any[] = [
+    {
+      title: "Angular",
+      icon: "../../assets/icons/angular.svg",
+      projectLinks: [
+        {
+          projectTitle: "Projeto final StartTech",
+          link: "https://google.com"
+        },
+        {
+          projectTitle: "Projeto final StartTech",
+          link: "https://google.com"
+        },
+        {
+          projectTitle: "Projeto final StartTech",
+          link: "https://google.com"
+        }
+      ]
+    },
+    {
+      title: "Java",
+      icon: "../../assets/icons/java.svg",
+      projectLinks: [
+        {
+          projectTitle: "Projeto projetado",
+          link: "https://google.com"
+        }
+      ]
+    }
+  ]
 }
