3a7d43dd313f14f3df2f7b58944f2ef3a9f788fc
Guilherme <francaguilherme27@gmail.com>
2023-11-16 11:19:06 -0300

    Testes unitarios

--- src/app/aside-menu/aside-menu.component.spec.ts
+++ src/app/aside-menu/aside-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AsideMenuComponent } from './aside-menu.component';
+import { AppModule } from '../app.module';
 
 describe('AsideMenuComponent', () => {
   let component: AsideMenuComponent;
@@ -8,6 +9,7 @@ describe('AsideMenuComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [AppModule],
       declarations: [AsideMenuComponent]
     });
     fixture = TestBed.createComponent(AsideMenuComponent);
@@ -15,7 +17,45 @@ describe('AsideMenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o AsideMenuComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('variavel "visible" deve inicializar como false', () => {
+    expect(component.visible).toBeFalse();
+  });
+
+  it('deve chamar os metodos toggleMenu e changeOpacity', () => {
+    spyOn(component, 'changeOpacity');
+
+    component.toggleMenu();
+
+    expect(component.visible).toBeTrue();
+    expect(component.changeOpacity).toHaveBeenCalled();
+    
+    component.toggleMenu();
+    
+    expect(component.visible).toBeFalse();
+    expect(component.changeOpacity).toHaveBeenCalled();
+  });
+
+  it('deve chamar o metodo closeMenu com "visible" true e false', () => {
+    component.closeMenu();
+
+    expect(component.visible).toBeFalse();
+
+    component.toggleMenu();
+    component.closeMenu();
+
+    expect(component.visible).toBeFalse();
+  });
+
+  it('deve chamar o metodo goTo e abrir o link em uma nova aba', () => {
+    const windowSpy = spyOn(window, 'open');
+    const linkTest = 'https://google.com'
+
+    component.goTo(linkTest);
+
+    expect(windowSpy).toHaveBeenCalledWith(linkTest, '_blank');
   });
 });
