9d24558a33222ea221add474438ded4f6420fe35
Guilherme <francaguilherme27@gmail.com>
2023-11-21 11:04:51 -0300

    Ajuste no formato do footer | responsividade

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -35,7 +35,30 @@ export class FooterComponent {
         }
       ]
     }
-  ]
+  ];
+
+  mainLinks: any[] = [
+        {
+      link: "https://www.linkedin.com/in/guilherme-fran%C3%A7a-da-silva-4756a8155",
+      icon: "../assets/icons/linkedin.svg",
+      description: "Linkedin"
+    },
+    {
+      link: "https://github.com/GuilhermeFdSilva",
+      icon: "../assets/icons/gitHub.svg",
+      description: "GitHub"
+    },
+    {
+      link: "mailto:francaguilherme27@gmail.com",
+      icon: "../assets/icons/email.svg",
+      description: "Email"
+    },
+    {
+      link: "https://drive.google.com/file/d/1tP8D-_z6kA6-VTLT6-dHS5_nEZ0yaQjF/view?usp=sharing",
+      icon: "../assets/icons/pdfFile.svg",
+      description: "Curriculo"
+    }
+  ];
 
   goTo(link: string): void {
     window.open(link, '_blank');
