82159488440ed288f6603a8f28069f7e96ad55a8
Guilherme <francaguilherme27@gmail.com>
2023-11-21 16:38:29 -0300

    Implementação de testes

--- src/app/container-router/container-router.component.spec.ts
+++ src/app/container-router/container-router.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContainerRouterComponent } from './container-router.component';
+import { AppModule } from '../app.module';
 
 describe('ContainerRouterComponent', () => {
   let component: ContainerRouterComponent;
@@ -8,6 +9,7 @@ describe('ContainerRouterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [AppModule],
       declarations: [ContainerRouterComponent]
     });
     fixture = TestBed.createComponent(ContainerRouterComponent);
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { AppModule } from '../app.module';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [AppModule],
       declarations: [HomeComponent]
     });
     fixture = TestBed.createComponent(HomeComponent);
@@ -15,7 +17,16 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o HomeComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve chamar o metodo goTo e abrir o link em uma nova aba', () => {
+    let windowSpy = spyOn(window, 'open');
+    let link = 'https://google.com';
+
+    component.goTo(link);
+
+    expect(windowSpy).toHaveBeenCalledWith(link, '_blank');
+  })
 });
