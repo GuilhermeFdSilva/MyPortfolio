87ec7e9718b03679fa54dde369092b5a06a2f699
Guilherme <francaguilherme27@gmail.com>
2023-11-22 16:44:58 -0300

    Rotas para lista de projetos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path: '', pathMatch:'full', redirectTo:'home'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'projetos', component: ProjectsComponent},
+  {path: 'projetos/:module', component: ProjectsComponent}
 ];
 
 @NgModule({
