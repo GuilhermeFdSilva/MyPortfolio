206c3e3062dbf5efd511eb384e760aeaccda6b01
Guilherme <francaguilherme27@gmail.com>
2023-11-23 16:05:40 -0300

    Migração para service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { LanguagesService } from './../../assets/services/languages/languages.service';
-import { Component } from '@angular/core';
+import { LanguagesService, Language } from './../../assets/services/languages/languages.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,86 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  languages: any[] = [
-    {
-      name: 'JavaScript',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-javascript.svg',
-      linkDesc: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-    },
-    {
-      name: 'Java',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-java.svg',
-      linkDesc: 'https://docs.oracle.com/en/java/',
-    },
-    {
-      name: 'C',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-c.svg',
-      linkDesc: 'https://www.ibm.com/docs/pt/i/7.2?topic=languages-c-c',
-    },
-    {
-      name: 'Python',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-python.svg',
-      linkDesc: 'https://wiki.python.org.br/PythonBrasil',
-    }
-  ]
+  languages: Array<Language> = [];
+  frontEnd: Array<Language> = [];
+  database: Array<Language> = [];
+  mainCategories: Array<Language> = []
 
-  frontEnd: any[] = [
-    {
-      name: 'HTML',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-html.svg',
-      linkDesc: 'https://developer.mozilla.org/pt-BR/docs/Web/HTML',
-    },
-    {
-      name: 'CSS',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-css.svg',
-      linkDesc: 'https://developer.mozilla.org/pt-BR/docs/Web/CSS',
-    },
-    {
-      name: 'SASS',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-sass.svg',
-      linkDesc: 'https://sass-lang.com/documentation/',
-    },
-    {
-      name: 'Bootstrap',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-bootstrap.svg',
-      linkDesc: 'https://getbootstrap.com.br/docs/4.1/getting-started/introduction/',
-    },
-    {
-      name: 'Angular',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-angular.svg',
-      linkDesc: 'https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_getting_started',
-    }
-  ]
+  constructor(private languagesService: LanguagesService) { }
 
-  database: any[] = [
-    {
-      name: 'SQL',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-mysql.svg',
-      linkDesc: 'https://dev.mysql.com/doc/',
-    },
-    {
-      name: 'MongoDB',
-      src: 'https://guilhermefdsilva.github.io/icons-myPortfolio/stick-mongodb.svg',
-      linkDesc: 'https://www.mongodb.com/docs/',
-    }
-  ]
-
-  categories: any[] = [
-    {
-      title: "Angular",
-      icon: "https://guilhermefdsilva.github.io/icons-myPortfolio/angular.svg",
-      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat praesentium aut quasi quidem saepe vel voluptate maxime autem et corporis est, facere dignissimos numquam nesciunt sunt nisi temporibus cumque aperiam!",
-      router: "home"
-    },
-    {
-      title: "Java",
-      icon: "https://guilhermefdsilva.github.io/icons-myPortfolio/java.svg",
-      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat praesentium aut quasi quidem saepe vel voluptate maxime autem et corporis est, facere dignissimos numquam nesciunt sunt nisi temporibus cumque aperiam!",
-      router: "home"
-    }
-  ]
-
-  constructor (private languagesService: LanguagesService) { }
+  ngOnInit(): void {
+    this.languagesService.getObservableData.subscribe(() => {
+      this.languages = this.languagesService.getLanguages.filter((lenguage) => lenguage.getType === 'PL');
+      this.frontEnd = this.languagesService.getLanguages.filter((lenguage) => lenguage.getType === 'FE');
+      this.database = this.languagesService.getLanguages.filter((lenguage) => lenguage.getType === 'DB');
+      this.mainCategories = this.languagesService.getLanguages.filter((language) => (language.getName === 'Angular') || (language.getName === 'Java'));
+    });
+  }
 
   goTo(link: string): void {
     window.open(link, '_blank');
