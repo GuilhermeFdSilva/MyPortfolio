bb93cb94ed80a5b241295361ce50b44566eaa221
Guilherme <francaguilherme27@gmail.com>
2023-11-23 16:05:01 -0300

    Atualização de rotas

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -36,24 +36,24 @@ export class FooterComponent {
   ];
 
   mainLinks: any[] = [
-        {
+    {
       link: "https://www.linkedin.com/in/guilherme-fran%C3%A7a-da-silva-4756a8155",
-      icon: "https://guilhermefdsilva.github.io/icons-myPortfolio/linkedin.svg",
+      icon: "https://guilhermefdsilva.github.io/read-db-myPortfolio/logos/linkedin.svg",
       description: "Linkedin"
     },
     {
       link: "https://github.com/GuilhermeFdSilva",
-      icon: "https://guilhermefdsilva.github.io/icons-myPortfolio/gitHub.svg",
+      icon: "https://guilhermefdsilva.github.io/read-db-myPortfolio/logos/gitHub.svg",
       description: "GitHub"
     },
     {
       link: "mailto:francaguilherme27@gmail.com",
-      icon: "https://guilhermefdsilva.github.io/icons-myPortfolio/email.svg",
+      icon: "https://guilhermefdsilva.github.io/read-db-myPortfolio/logos/email.svg",
       description: "Email"
     },
     {
       link: "https://drive.google.com/file/d/1tP8D-_z6kA6-VTLT6-dHS5_nEZ0yaQjF/view?usp=sharing",
-      icon: "https://guilhermefdsilva.github.io/icons-myPortfolio/pdfFile.svg",
+      icon: "https://guilhermefdsilva.github.io/read-db-myPortfolio/logos/pdfFile.svg",
       description: "Curriculo"
     }
   ];
