acb3810b6070854ed258f793f359bf8eadc398c2
Guilherme <francaguilherme27@gmail.com>
2023-11-28 11:53:59 -0300

    Ajuste de nome de função

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent {
     window.open(link, '_blank');
   }
 
-  goToComponent(route: string) {
+  goToPorjects(route: string) {
     this.router.navigate([`/projetos/${route.toLowerCase()}`]);
   }
 }
