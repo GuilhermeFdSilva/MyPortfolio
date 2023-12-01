f67b8e8c7962361c46a8637734d3869bd3f5478f
Guilherme <francaguilherme27@gmail.com>
2023-12-01 15:56:59 -0300

    Construção da service readme

--- src/assets/services/readme/readme.service.ts
+++ src/assets/services/readme/readme.service.ts
@@ -1,9 +1,38 @@
+import { Subject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReadmeService {
+  private readme: string;
 
-  constructor() { }
+  private observableData = new Subject<void>();
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProjectReadme(projectName: string) {
+    this.httpClient.get<any>(`https://api.github.com/repos/GuilhermeFdSilva/${projectName}/contents/README.md`)
+      .subscribe((response) => {
+        this.readme = this.decode(response.content);
+        this.observableData.next();
+      });
+  }
+
+  private decode(content: string): string {
+    const binaryString = atob(content);
+    
+    const textReadme = new TextDecoder('utf-8').decode(new Uint8Array([...binaryString].map(char => char.charCodeAt(0))));
+    
+    return textReadme;
+  }
+
+  get getReadme(): string {
+    return this.readme;
+  }
+
+  get getObservableData(): Observable<void> {
+    return this.observableData.asObservable();
+  }
 }
