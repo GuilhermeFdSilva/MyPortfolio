20bc818cceb05f664f1dd0097e6229f015216fcb
Guilherme <francaguilherme27@gmail.com>
2023-12-01 13:38:57 -0300

    Gerenciamento do projeto selecionado

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { LanguagesService } from './../../assets/services/languages/languages.service';
+import { ProjectsService } from './../../assets/services/projects/projects.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/assets/services/projects/projects.service';
+import { Language } from 'src/assets/services/languages/languages.service';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
+  project: Project;
+  languages: Array<Language>;
 
+  constructor (private activatedRoute: ActivatedRoute, private projectsService: ProjectsService, private languagesService: LanguagesService) { }
+
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+
+    this.activatedRoute.paramMap.subscribe((param) => {
+      const id = param.get('id') ?? '';
+
+      let projectFind = this.projectsService.getProjects.find((project) => project.getId == Number.parseInt(id));
+      this.project = projectFind ? projectFind : new Project();
+      this.projectsService.getObservableData.subscribe(() => {
+        projectFind = this.projectsService.getProjects.find((project) => project.getId == Number.parseInt(id));
+        this.project = projectFind ? projectFind : new Project();
+      });
+    });
+
+    this.languages = this.languagesService.getLanguages;
+    this.languagesService.getObservableData.subscribe(() => {
+      this.languages = this.languagesService.getLanguages;
+    });
+  }
 }
