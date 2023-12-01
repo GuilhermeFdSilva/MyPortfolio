31118f99dab7affd81dfca14c32f2cfd921ff5be
Guilherme <francaguilherme27@gmail.com>
2023-12-01 15:56:17 -0300

    Implementação Readme service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,9 +1,11 @@
+import { ReadmeService } from './../../assets/services/readme/readme.service';
 import { LanguagesService } from './../../assets/services/languages/languages.service';
 import { ProjectsService } from './../../assets/services/projects/projects.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/assets/services/projects/projects.service';
 import { Language } from 'src/assets/services/languages/languages.service';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-details',
@@ -11,10 +13,11 @@ import { Language } from 'src/assets/services/languages/languages.service';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
-  project: Project;
-  languages: Array<Language>;
+  project: Project = new Project();
+  languages: Array<Language> = [];
+  readmeContent: string | Promise<string>;
 
-  constructor (private activatedRoute: ActivatedRoute, private projectsService: ProjectsService, private languagesService: LanguagesService) { }
+  constructor(private activatedRoute: ActivatedRoute, private projectsService: ProjectsService, private languagesService: LanguagesService, private readmeService: ReadmeService) { }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
@@ -27,6 +30,11 @@ export class DetailsComponent {
       this.projectsService.getObservableData.subscribe(() => {
         projectFind = this.projectsService.getProjects.find((project) => project.getId == Number.parseInt(id));
         this.project = projectFind ? projectFind : new Project();
+
+        this.readmeService.getProjectReadme(this.project.getNameGH);
+        this.readmeService.getObservableData.subscribe(() => {
+          this.readmeContent = marked(this.readmeService.getReadme);
+        });
       });
     });
 
