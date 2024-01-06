69082d09aff85af3c793060a0d454b60c7d0c3a9
Guilherme <francaguilherme27@gmail.com>
2024-01-06 19:36:43 -0300

    Componente filho para apresentação

---
+++ src/app/home/presentation/presentation.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PresentationComponent } from './presentation.component';
+
+describe('PresentationComponent', () => {
+  let component: PresentationComponent;
+  let fixture: ComponentFixture<PresentationComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PresentationComponent]
+    });
+    fixture = TestBed.createComponent(PresentationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/home/presentation/presentation.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Language } from 'src/assets/service/dataManagerService/languages/languages.service';
+
+@Component({
+  selector: 'app-presentation',
+  templateUrl: './presentation.component.html',
+  styleUrls: ['./presentation.component.scss']
+})
+export class PresentationComponent {
+  @Input() languages: Array<Language> = [];
+  @Input() frontEnd: Array<Language> = [];
+  @Input() database: Array<Language> = [];
+
+  @Output() goTo = new EventEmitter<any>();
+
+  navigate(link: string): void {
+    this.goTo.emit(link);
+  }
+}
