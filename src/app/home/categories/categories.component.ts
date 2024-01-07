f5469470943a92cbc9d0d7af8e1ce40414149513
Guilherme <francaguilherme27@gmail.com>
2024-01-06 21:29:14 -0300

    Estruturação

---
+++ src/app/home/categories/categories.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Language } from 'src/assets/service/dataManagerService/languages/languages.service';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss']
+})
+export class CategoriesComponent {
+  @Input() mainCategories: Array<Language> = [];
+
+  scroll(id: string): void {
+    let target = document.getElementById(id);
+
+    if (target) {
+      target.scrollIntoView({behavior: "smooth"});
+    }
+  }
+}
