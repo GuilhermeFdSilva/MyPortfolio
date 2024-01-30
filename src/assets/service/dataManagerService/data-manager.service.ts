fabd7b55e813a477653e68b7aba863a424afb1f1
Guilherme <francaguilherme27@gmail.com>
2024-01-30 15:10:25 -0300

    Funções de comentario | implementação de service

--- src/assets/service/dataManagerService/data-manager.service.ts
+++ src/assets/service/dataManagerService/data-manager.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, forkJoin } from 'rxjs';
 import { Language, LanguagesService } from './languages/languages.service';
 import { Project, ProjectsService } from './projects/projects.service';
+import { Comment, CommentsService } from './comments/comments.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,17 @@ import { Project, ProjectsService } from './projects/projects.service';
 export class DataManagerService {
   private languages: Array<Language> = [];
   private projects: Array<Project> = [];
+  private comments: Array<Comment> = [];
 
   private observableData = new BehaviorSubject<boolean>(false);
+  private observableComments = new BehaviorSubject<boolean>(false);
 
   constructor(
     private languagesService: LanguagesService,
     private projectsService: ProjectsService,
+    private commentsService: CommentsService
   ) {
     this.loadLanguagesAndProjects();
-  }
-
-  get getObservableData(): Observable<boolean> {
-    return this.observableData.asObservable();
   }
 
   get getLanguages(): Array<Language> {
@@ -29,6 +29,18 @@ export class DataManagerService {
 
   get getProjects(): Array<Project> {
     return this.projects;
+  }
+
+  get getComments(): Array<Comment> {
+    return this.comments;
+  }
+
+  get getObservableData(): Observable<boolean> {
+    return this.observableData.asObservable();
+  }
+
+  get getObservableComments(): Observable<boolean> {
+    return this.observableComments.asObservable();
   }
 
   loadLanguagesAndProjects(): void {
@@ -69,5 +81,29 @@ export class DataManagerService {
 
       this.observableData.next(true);
     });
+  }
+
+  loadComments(project_id: number): void {
+    this.commentsService.getCommentsByProjectId(project_id).subscribe((response) => {
+      this.comments = response.map((comment) => Object.assign(new Comment, comment));
+
+      this.observableComments.next(true);
+    });
+  }
+
+  commentUpVote(target: Comment, voted: boolean): void {
+    if (voted) {
+      this.commentsService.removeVoteUp(target).subscribe(() => this.observableComments.next(true));
+    } else {
+      this.commentsService.voteUp(target).subscribe(() => this.observableComments.next(true));
+    }
+  }
+
+  commentDownVote(target: Comment, voted: boolean): void {
+    if (voted) {
+      this.commentsService.removeVoteDown(target).subscribe(() => this.observableComments.next(true));
+    } else {
+      this.commentsService.voteDown(target).subscribe(() => this.observableComments.next(true));
+    }
   }
 }
